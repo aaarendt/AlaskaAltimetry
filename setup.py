7fd70f9e9e0e8c81f355cd1be7865f8158db073d
ice2ocean <arendta@uw.edu>
2015-11-04 20:40:24 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'Altimetry',
+    packages = ['Altimetry'],
+    version = '1.0',
+    description = 'Software for querying and analyzing UAF Altimetry data on a posgres database platform such as ice2oceans.cloudapp.net',
+    author='Evan Burgess',
+    author_email='evanburgess@gmail.com',
+    url='https://github.com/evanburgess/IceBridgeAlaskaAltimetry',
+    py_modules=['Altimetry'],
+   )
